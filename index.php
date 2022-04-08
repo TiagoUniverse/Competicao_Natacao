6bbd89149de6d047efd2a7c76711639e961103ae
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-08 11:36:39 -0300

    Criei a analise de condição com estrutura if

--- index.php
+++ index.php
@@ -14,7 +14,18 @@
     $idade = '20';
 
     //Var_dump exibe o que tem armazenado na variável
-    var_dump($nome);
-    var_dump($idade);
+    //Deduz o tipo da variável
+    //var_dump($nome);
+    //var_dump($idade);
+
+    if($idade >=6 && $idade <=  12){
+        echo  "$nome, com $idade anos, pertence à categoria: Infantil";
+    }elseif($idade >= 13 && $idade <= 18 ){
+        echo  "$nome, com $idade anos, pertence à categoria: Adolescente";
+    }elseif($idade > 18){
+       echo  "$nome, com $idade anos, pertence à categoria: Adulto";
+    }else{
+        echo 'Idade mínina de 6 anos!';
+    }
 
 ?>
