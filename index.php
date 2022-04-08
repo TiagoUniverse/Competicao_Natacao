e4cc9d964ea3914eb496066e101afbc77fba239f
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-08 11:19:43 -0300

    teste de var-dump com novas variáveis

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
-   //Variváveis
    // Criando um array
    $categoria = [];
    $categoria [] = 'infantil';
    $categoria [] = 'adolescente';
    $categoria [] = 'adulto';
+   $categoria [] = 'idoso';
 
-    print_r($categoria);
+   // print_r($categoria);
     
+    //Variváveis
+    $nome;
+    $idade;
+
+    var_dump($nome);
+    var_dump($idade);
 
 ?>
