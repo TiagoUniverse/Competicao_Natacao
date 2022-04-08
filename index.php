1a7b3fa920b830727b95a9dd0d75d574c4219921
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-08 12:09:00 -0300

    Corrigi a estrutura do if para classificar um nadador

--- index.php
+++ index.php
@@ -18,12 +18,35 @@
     //var_dump($nome);
     //var_dump($idade);
 
-    if($idade >=6 && $idade <=  12){
-        echo  "$nome, com $idade anos, pertence à categoria: Infantil";
-    }elseif($idade >= 13 && $idade <= 18 ){
-        echo  "$nome, com $idade anos, pertence à categoria: Adolescente";
-    }elseif($idade > 18){
-       echo  "$nome, com $idade anos, pertence à categoria: Adulto";
+
+    if($idade >=6 && $idade <= 12){
+       for($i = 0; $i <= count($categoria); $i++){
+           if($categoria[$i] == 'infantil'){
+              echo  "O nadador $nome, com $idade anos, pertence à categoria: Infantil";  
+              break;
+           }    
+       }
+    }elseif($idade >=13 && $idade <=18){
+        for($i = 0; $i <= count($categoria); $i++){
+            if($categoria[$i] == 'adolescente'){
+               echo  "O nadador $nome, com $idade anos, pertence à categoria: Adolescente";  
+               break;
+            }    
+        }
+    }elseif($idade >18 && $idade <=60){
+        for($i = 0; $i <= count($categoria); $i++){
+            if($categoria[$i] == 'adulto'){
+               echo  "O nadador $nome, com $idade anos, pertence à categoria: Adulto";  
+               break;
+            }    
+        }        
+    }elseif($idade >60){
+        for($i = 0; $i <= count($categoria); $i++){
+            if($categoria[$i] == 'idoso'){
+               echo  "O nadador $nome, com $idade anos, pertence à categoria: Idoso";  
+               break;
+            }    
+        }        
     }else{
         echo 'Idade mínina de 6 anos!';
     }
